08c1692a65d7fc8d119d742d47232735f5bf6b66
Jason Kubo <kuboj86@gmail.com>
2018-01-23 18:13:00 -0500

    push files

---
+++ AldiScheduleMaker/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+using System.Web.DynamicData;
+
+using AldiScheduleMaker.Models;
+using System.Data.Entity;
+
+namespace AldiScheduleMaker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ShowSchedule()
+        {
+            ScheduleController DAL = new ScheduleController();
+
+            ViewBag.EmployeeList = DAL.GetAllEmployees();
+            return View("ShowSchedule");
+        }
+    }
+}
---
+++ AldiScheduleMaker/Controllers/ScheduleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AldiScheduleMaker.Models;
+
+
+namespace AldiScheduleMaker.Controllers
+{
+    public class ScheduleController
+    {
+        private Aldi_EmployeesEntities ORM = new Aldi_EmployeesEntities();
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> EmployeeList = ORM.Employees.ToList();
+            return EmployeeList;
+        }
+    }
+}
